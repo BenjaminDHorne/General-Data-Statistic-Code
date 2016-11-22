7e21d5ec06c6f4c345f6662f23da2e8bafbb24fa
Benjamin D Horne <benjamindhorne314@gmail.com>
2016-11-22 17:08:49 -0500

    Update and rename NormalData_SignificantDifferenceTests.R to SignificantDifferenceTests.R
    added non-normal data test

--- SignificantDifferenceTests.R
+++ SignificantDifferenceTests.R
@@ -1,17 +1,20 @@
 mydata <- read.csv("",stringsAsFactors = F)
 
 #create a side-by-side boxplot of data by type, change feature looking at if needed
-boxplot(mydatawithtype$Tone~mydatawithtype$utype, notch=FALSE)
+boxplot(mydata$feat1~mydata$type, notch=FALSE)
 
 #anova, for balanced groups, if your data is normal
-myova <- aov(mydatawithtype$compare ~ mydatawithtype$utype)
+myova <- aov(mydata$feat1 ~ mydata$type)
 summary(myova)
 
 #Tukey Test
 TukeyHSD(myova)
 
 #try a pairwise t test for comparision 
-pairwise.t.test(mydatawithtype$compare, mydatawithtype$utype, p.adj = "none")
+pairwise.t.test(mydata$feat1, mydata$type, p.adj = "none")
+
+#Wilcox Test, if the data is not normal
+wilcox.test(feat1 ~ type, data = mydata)
 
 #correlations, dont need this all the time, just a check to see if any of the features are highly dependent
 #this requires manual inspection and removal of highly correlated variables before proceeding
